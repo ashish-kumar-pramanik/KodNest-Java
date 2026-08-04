f6ff26523b78f794e4de04bf0cd5f278b0596ee7
ashu singh <ashishkumarpramamnik20004@gmail.com>
2026-08-04 12:01:11 +0530

    feat: add show class to demonstrate print and println statements

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/show.java
@@ -0,0 +1,11 @@
+class show {
+    public static void main(String[] args) {
+        System.out.print("Monday");
+        System.out.println("Tuesday");
+        System.out.print("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.print("Saturday");
+        System.out.println("Sunday");
+    }
+}
