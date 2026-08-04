fc5fdda6645cdf5a78579095c4fb8bfed2a687da
ashu singh <ashishkumarpramamnik20004@gmail.com>
2026-08-04 12:26:56 +0530

    positive/negative

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/positive.java
@@ -0,0 +1,16 @@
+ public class positive {
+    public static void main(String[] args) {
+        int num=23;
+        if (num > 0){
+            System.out.println("positive");
+        }
+        else if(num==0){
+            System.out.println("zero");
+        }
+        else{
+            System.out.println("negative");
+        }
+        
+    }
+    
+}
